039e7a17b3373dfd0d1cad11e4bcb134aaf0dae5
henri-kok <129105070+henri-kok@users.noreply.github.com>
2025-10-28 13:23:13 +0200

    Added css for searchresults

--- search.php
+++ search.php
@@ -1,26 +1,24 @@
 <?php
 get_header();
 ?>
-
-<main id="content" role="main">
-    <header class="search-header">
-        <h1>
-            <?php printf( __( 'Zoekresultaten voor: %s', 'textdomain' ), '<span>' . get_search_query() . '</span>' ); ?>
-        </h1>
-    </header>
-
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <article <?php post_class(); ?>>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <?php the_excerpt(); ?>
-            </article>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <p><?php _e('Geen resultaten gevonden.', 'textdomain'); ?></p>
-        <?php get_search_form(); ?>
-    <?php endif; ?>
-</main>
+    
+        <section class="search-results with-margin">
+            <div class="wrapper">
+            <h1><?php printf( __( 'Zoekresultaten voor: %s', 'textdomain' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+            <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article class="search-result-item">
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_excerpt(); ?>
+                        <a class="btn" href="<?php the_permalink(); ?>">Lees meer</a>
+                    </article>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <p><?php _e('Geen resultaten gevonden.', 'textdomain'); ?></p>
+                <?php get_search_form(); ?>
+            <?php endif; ?>
+        </div>
+    </section>
 
 <?php
 get_footer();
