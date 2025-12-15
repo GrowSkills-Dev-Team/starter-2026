afd916f00a4e3434d03f4861a3bc228b5c357e8f
henri-kok <129105070+henri-kok@users.noreply.github.com>
2025-12-15 08:45:53 +0200

    removed ACF fields in sidebar in admin

--- blocks/body/render.php
+++ blocks/body/render.php
@@ -1,9 +1,11 @@
 <?php
 $show_title = get_field('show_title');
 $body = get_field('body');
-$button = get_field('button');
 $name_spacing = 'margin';
 $spacing = get_field('spacing');
+
+$button_label = get_field('button_label');
+$button_link = get_field('button_link');
 
 if($show_title || !empty($body)) : ?>
     <section class="body-text<?= ($spacing === 'none' ? ' no-' . $name_spacing : ' with-' . $name_spacing); ?><?= ($spacing ? ' with-' . $name_spacing . '-' . $spacing : ''); ?>">
@@ -11,7 +13,7 @@ if($show_title || !empty($body)) : ?>
             <div class="body-text-text text-container">
                 <?= ($show_title ? '<h1>' . get_the_title() . '</h1>' : null); ?>
                 <?= $body; ?>
-                <?= ($button ? '<a class="btn" href="'. $button['url'] .'">'. $button['title'] .'</a>' : null); ?>
+                <?= ($button_label ? '<a class="btn" href="#">' . $button_label . '</a>' : null); ?>
             </div>
         </div>
     </section>
